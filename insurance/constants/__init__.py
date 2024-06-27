2a7afb40c091bb2ae49c1499bda7df7707df147c
Dhruv Saxena <saxena25dhruv@gmail.com>
2024-06-27 22:18:08 +0530

    commit

---
+++ insurance/constants/__init__.py
@@ -0,0 +1,8 @@
+import os
+from datetime import date
+
+DATABASE_NAME = "internship"
+
+COLLECTION_NAME = "insurance_data"
+
+MONGODB_URL_KEY = "mongodb+srv://dhruv:DHRUV@cluster0.clcbsrx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" #"MONGODB_URL"
