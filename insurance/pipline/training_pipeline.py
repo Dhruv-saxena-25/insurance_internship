2b3a5e990c6693684ff3a723c03b89929234082a
Dhruv Saxena <saxena25dhruv@gmail.com>
2024-06-30 00:41:51 +0530

    Data_Validation

--- insurance/pipline/training_pipeline.py
+++ insurance/pipline/training_pipeline.py
@@ -4,13 +4,17 @@ from insurance.logger import logging
 
 
 from insurance.components.data_ingestion import DataIngestion
+from insurance.components.data_validation import DataValidation
 
-from insurance.entity.config_entity import (DataIngestionConfig)
-from insurance.entity.artifact_entity import (DataIngestionArtifact)
+from insurance.entity.config_entity import (DataIngestionConfig,
+                                            DataValidationConfig)
+from insurance.entity.artifact_entity import (DataIngestionArtifact,
+                                              DataValidationArtifact)
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
+        self.data_validation_config = DataValidationConfig()
 
     def start_data_ingestion(self)-> DataIngestionArtifact:
         
@@ -29,9 +33,28 @@ class TrainPipeline:
         except Exception as e:
             raise InsuranceException(e, sys) from e
         
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact)-> DataValidationArtifact:
+        try:
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+                                             data_validation_config=self.data_validation_config
+                                             )
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            logging.info("Performed the data validation operation")
+
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
+
+            return data_validation_artifact
+
+        except Exception as e:
+            raise InsuranceException(e, sys) from e
 
     def run_pipeline(self,) -> None:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
         except Exception as e:
             raise InsuranceException(e, sys) from  e
